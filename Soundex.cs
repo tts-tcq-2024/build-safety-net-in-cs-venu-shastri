95c6521a991b94f78a135446b216ca8937d8a1d6
Venugopal K <venu.shastri@gmail.com>
2024-06-26 23:45:59 +0530

    Update and rename checker.cs to Soundex.cs

---
+++ Soundex.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text;
+
+public class Soundex
+{
+    public static string GenerateSoundex(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            return string.Empty;
+        }
+
+        StringBuilder soundex = new StringBuilder();
+        soundex.Append(char.ToUpper(name[0]));
+        char prevCode = GetSoundexCode(name[0]);
+
+        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
+        {
+            char code = GetSoundexCode(name[i]);
+            if (code != '0' && code != prevCode)
+            {
+                soundex.Append(code);
+                prevCode = code;
+            }
+        }
+
+        while (soundex.Length < 4)
+        {
+            soundex.Append('0');
+        }
+
+        return soundex.ToString();
+    }
+
+    private static char GetSoundexCode(char c)
+    {
+        c = char.ToUpper(c);
+        switch (c)
+        {
+            case 'B':
+            case 'F':
+            case 'P':
+            case 'V':
+                return '1';
+            case 'C':
+            case 'G':
+            case 'J':
+            case 'K':
+            case 'Q':
+            case 'S':
+            case 'X':
+            case 'Z':
+                return '2';
+            case 'D':
+            case 'T':
+                return '3';
+            case 'L':
+                return '4';
+            case 'M':
+            case 'N':
+                return '5';
+            case 'R':
+                return '6';
+           
